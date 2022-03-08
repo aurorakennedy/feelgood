ba29725ce1a99ce8c4a1123c24817390bfff58bd
Aurora Kennedy <aurorke@ntnu.no>
2022-03-08 12:15:53 +0100

    person konstruktor og attributter

--- src/main/java/feelgood/Person.java
+++ src/main/java/feelgood/Person.java
@@ -1,5 +1,31 @@
 package feelgood;
 
+import java.util.Date;
+
+
+    // Lagre person - Skal vi ha muligheen til å lage ny person - reste alt 
+    // Ta inn repiterende info 
+
 public class Person {
     private String name;
+    private Date bday; 
+    private String gender; //Woke alternativer!!
+
+
+    // Konstruktør 
+    public Person(String name, Date bday, String gender){
+
+        this.name = name; 
+        this.bday = bday;
+        this.gender = gender; 
+
+
+    }
+
+    // Lagre atributer til fil 
+
+
+
+    
+
 }
