865c690663b43e1b9cd0d5b0be5c427d9a26ba2c
Aurora Kennedy <aurorke@ntnu.no>
2022-03-22 10:40:12 +0100

    Controler mal

--- src/main/java/feelgood/FeelGoodController.java
+++ src/main/java/feelgood/FeelGoodController.java
@@ -4,4 +4,19 @@ public class FeelGoodController {
     //overføres data fra fxml til variabler vi kan bruke i andre klasser
     //starte metoder i de andre klassene
     //som en main-metode
+
+    private FeelGoodApp feelGoodApp;  //Kan hende det skal være day og person her ikke Feelgoodapp
+
+    // Kobler opp FXML med controlleren 
+
+    @FXML 
+    public idNavnHer itemNavn; //Bytt ut med id 
+
+    // Trenger ikke konstruktør 
+    
+    public void initialize() {
+        // Dette er på en måte konstruktøren vår -- kjører når appen starter opp
+    }
+
+
 }
